9b2e94ab6452b0303bba205e1bee8feafd12e074
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2024-09-25 14:56:25 +0100

    Feat: add film crud

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
+const filmController = require('../controllers/filmController');
 
 router.get('/', (req, res) => {
     res.send('Welcome to CinéManager!');
@@ -11,7 +11,13 @@ router.get('/', (req, res) => {
 router.post('/api/auth/register', authController.register);
 router.post('/api/auth/login', authController.login);
 
+// film crud
 
+router.post('/api/film/create', filmController.createfilm);
+router.get('/api/film/getAll', filmController.getAllfilm);
+router.get('/api/film/getFilm/:id', filmController.getfilmById);
+router.put('/api/film/editFilm/:id', filmController.updateFilm);
+router.delete('/api/film/deleteFilm/:id', filmController.deleteFilm);
 
 
 module.exports = router;
